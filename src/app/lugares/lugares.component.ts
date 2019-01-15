4255b21bd42e6e83f277e8530433cfc4bc15a5ef
Camilo Rodriguez <robcamilo@hotmail.com>
2019-01-14 22:18:39 -0500

    parametros en rutas

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 export class LugaresComponent {
     title = 'platzisquare';
     lugares: any = [
-        {clicks: '', plan: 'Pagado', cercania: 1, distancia: 1, active: true, nombre: 'Floreria la Gardenia'},
-        {clicks: '', plan: 'Gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita'},
-        {clicks: '', plan: 'Gratuito', cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria Huellitas felices'},
-        {clicks: '', plan: 'Gratuito', cercania: 3, distancia: 10, active: false, nombre: 'Sushi Sushiroll'},
-        {clicks: '', plan: 'Pagado', cercania: 3, distancia: 35, active: true, nombre: 'Hotel la Gracia'},
-        {clicks: '', plan: 'Gratuito', cercania: 3, distancia: 10, active: false, nombre: 'Zapateria el Clavo'}
+        {id: 1, clicks: '', plan: 'Pagado', cercania: 1, distancia: 1, active: true, nombre: 'Floreria la Gardenia'},
+        {id: 2, clicks: '', plan: 'Gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas la pasadita'},
+        {id: 3, clicks: '', plan: 'Gratuito', cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria Huellitas felices'},
+        {id: 4, clicks: '', plan: 'Gratuito', cercania: 3, distancia: 10, active: false, nombre: 'Sushi Sushiroll'},
+        {id: 5, clicks: '', plan: 'Pagado', cercania: 3, distancia: 35, active: true, nombre: 'Hotel la Gracia'},
+        {id: 6, clicks: '', plan: 'Gratuito', cercania: 3, distancia: 10, active: false, nombre: 'Zapateria el Clavo'}
     ];
     lat:number = 4.7186655;
     lng:number = -74.0356525;
